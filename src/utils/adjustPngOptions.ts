10d837a6d06634d2c88f0c61e8cb2270205837c6
Florian Körner <contact@florian-koerner.com>
2021-11-16 22:26:26 +0000

    Fix: Max Size

--- src/utils/adjustPngOptions.ts
+++ src/utils/adjustPngOptions.ts
@@ -1,9 +1,11 @@
 export function adjustPngOptions(options: Record<string, any>) {
   // Validate Size for PNG Format
-  const size = options['size'] || options['w'] || options['width'] || options['h'] || options['height'];
+  const size = parseInt(options['size'] || options['w'] || options['width'] || options['h'] || options['height'] || '0');
   const minSize = parseInt(process.env.MIN_PNG_SIZE ?? '0') || 1;
   const maxSize = parseInt(process.env.MAX_PNG_SIZE ?? '0') || 128;
   const validatedSize = size < minSize || size > maxSize ? maxSize : size;
+
+console.log({size, minSize, maxSize, validatedSize});
 
   delete options['size'];
   delete options['w'];
