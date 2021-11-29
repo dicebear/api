d615459fa1c8f4b0df1f1bd3e8d6034631081761
Florian Körner <contact@florian-koerner.com>
2021-11-29 12:33:59 +0000

    Fix: Route

--- versions/5.0/index.ts
+++ versions/5.0/index.ts
@@ -2,9 +2,9 @@ import { createAvatar, schema } from '@dicebear/core';
 import * as styles from '@dicebear/collection';
 
 const routes = [
-  '/5.9/:style/:format',
+  '/5.0/:style/:format',
   '/5.x/:style/:format',
-  '/5.9/:style/:seed/:format',
+  '/5.0/:style/:seed/:format',
   '/5.x/:style/:seed/:format',
 ];
 
