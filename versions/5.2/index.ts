ad2c60703f36bc741d4cf9b9481bf7c4bc341ded
Florian Körner <contact@florian-koerner.com>
2023-10-10 23:04:15 +0200

    Remove: Versions folder

--- versions/5.2/index.ts
+++
@@ -1,9 +0,0 @@
-import { createAvatar, schema, license } from '@dicebear/core';
-import * as styles from '@dicebear/collection';
-import { Routes } from '../../types';
-
-const exif = license.exif;
-
-const routes: Routes = ['/5.2/:style/:format', '/5.2/:style/:format/:options'];
-
-export { routes, createAvatar, schema, styles, exif };
